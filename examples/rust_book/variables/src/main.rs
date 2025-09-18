268f1b41a44d171d80b98977405629a3f26d451e
Josh Walker <joshwalker.oni@gmail.com>
2025-09-18 20:49:11 +0100

    variables section complete

--- examples/rust_book/variables/src/main.rs
+++ examples/rust_book/variables/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     x = 6;
     println!("The value of x is: {x}");
 
+
+    // Shadowing
+    let y = 5;
+
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}")
+
+    }
+
+    println!("The value of y is: {y}")
+
 }
 
-const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60
+const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
