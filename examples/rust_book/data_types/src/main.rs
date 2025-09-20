84c8bc0031d56b6d92f74984d14a8a9530c09b45
Josh Walker <joshwalker.oni@gmail.com>
2025-09-20 11:30:35 +0100

    scaler data_types

--- examples/rust_book/data_types/src/main.rs
+++ examples/rust_book/data_types/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+
+    // floating integer types
+    let x = 2.0; // f64
+
+    let y: u32 = 3.0; // f32
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; //Result is -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // booleans
+    let t = true;
+
+    let f: blool
+
+    // character type - literals with single quotes
+    let c = 'z';
+    let x: char = 'Z';
+    let heart_eyed_cat = '😻';
+
+    // tuple types
+    let tup: 
+
+
+
 }
