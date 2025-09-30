e47f6bb84cce9f0a0173a680ed90a86f291e32cc
Josh Walker <joshwalker.oni@gmail.com>
2025-09-30 13:14:11 +0100

    finished temp_converter from farenheight to celcius

--- examples/rust_book/temp_converter/src/main.rs
+++ examples/rust_book/temp_converter/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut temp = String::new();
+
+    println!("Farenheight to Celsius converter!");
+    println!("Please enter the temperature in farenheight:");
+
+    io::stdin()
+        .read_line(&mut temp)
+        .expect("Failed to read line");
+
+    let temp: f64 = match temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Not a valid number!");
+            return;
+        }
+    };
+
+    println!("Farenheight: {temp}");
+    
+    let celc: f64 = (temp - 32.0) * (5.0/9.0);
+
+    println!("Celcius: {:.2}", celc);
+
 }
