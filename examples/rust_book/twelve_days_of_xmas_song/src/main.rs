e363ea399c3be7d02c71a719bed771d0b9c8dd5b
Josh Walker <joshwalker.oni@gmail.com>
2025-09-30 20:31:32 +0100

    song done!

---
+++ examples/rust_book/twelve_days_of_xmas_song/src/main.rs
@@ -0,0 +1,29 @@
+
+const ORDINALS: [&str; 12] = [
+    "first", "second", "third", "fourth", "fifth", "sixth",
+    "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
+];
+
+const GIFTS: [&str; 12] = [
+    "a Partridge in a Pear Tree",
+    "Two Turtle Doves",
+    "Three French Hens",
+    "Four Calling Birds",
+    "Five Gold Rings",
+    "Six Geese a Laying",
+    "Seven Swans a Swimming",
+    "Eight Maids a Milking",
+    "Nine Ladies Dancing",
+    "Ten Lords a Leaping",
+    "Eleven Pipers Piping",
+    "Twelve Drummers Drumming"
+];
+
+fn main() {
+    for day in 0..11 {
+        println!("On the {} day of christmas my true love gave to me:", ORDINALS[day]);
+        for gift in (0..=day).rev() {
+            println!("{}", GIFTS[gift]);
+            };
+        };
+    }
