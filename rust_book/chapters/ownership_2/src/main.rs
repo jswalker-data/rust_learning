6e72dd9b20a517e55c149fb3802160aa6603ea4e
Josh Walker <joshwalker.oni@gmail.com>
2025-10-23 21:16:20 +0100

    second script for the ownership examples

--- rust_book/chapters/ownership_2/src/main.rs
+++ rust_book/chapters/ownership_2/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    
+    // s comes into scope
+    let s = String::from("hello");
+    
+    // s's value moves into the function and is no longer valid here
+    takes_ownership(s);
+
+    // x comes into scope
+    let x = 5;
+
+    // because i32 implements the Copy trait, x does NOT move into
+    // the function, so it's okay to use x afterwards
+    makes_copy(x);
+
+    // at this point s is gone and x still exists
+
+// here, x goes out of scope, then s
+// however s value was moved so nothing happens
 }
+
+fn takes_ownership(some_string: String) {
+    
+    // some_string comes into scope
+    println!("{some_string}");
+
+// some_string goes out of scope and 'drop' called
+// backing memory is freed
+}
+
+fn makes_copy(some_integer: i32) {
+
+    // some_integer comes into scope
+    println!("{some_integer}");
+
+// some_integer goes out of scope, nothing happens
+}
+
+
+
+
