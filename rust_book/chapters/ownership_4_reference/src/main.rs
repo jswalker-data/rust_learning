942e794e6ea053ba322a771c59f5315b871ba406
Josh Walker <joshwalker.oni@gmail.com>
2025-10-24 15:25:47 +0100

    addednotes around the scope and ownership

--- rust_book/chapters/ownership_4_reference/src/main.rs
+++ rust_book/chapters/ownership_4_reference/src/main.rs
@@ -10,6 +10,10 @@ fn main() {
     println!("The length of '{s1}' is {len}.")
 }
 
+// s is a reference to a String
 fn calculate_length(s: &String) -> usize {
     s.len()
+
+// Here, s goes out of scope. But because s does not have ownership of 
+// what it refers to, the String is not dropped
 }
