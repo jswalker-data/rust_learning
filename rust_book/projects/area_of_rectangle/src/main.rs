c0d8d22d0368b8bc2baf4a98234798bb9d6a45c6
Josh Walker <joshwalker.oni@gmail.com>
2025-10-29 23:34:34 +0000

    refactored to using a struct for rectangle and using its fields

--- rust_book/projects/area_of_rectangle/src/main.rs
+++ rust_book/projects/area_of_rectangle/src/main.rs
@@ -7,16 +7,26 @@
 // refactor: change dual input to a single input, we can't see that
 // these are related in any ways at the moment
 
+// refactor: the tuple doesn't give us any info on what is height and
+/// what is width. let's change to a struct for more information
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} pixels!",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
