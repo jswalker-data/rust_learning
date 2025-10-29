5e707f8b7625a43e341a3c30191703299cacd238
Josh Walker <joshwalker.oni@gmail.com>
2025-10-29 23:46:59 +0000

    added printing/display

--- rust_book/projects/area_of_rectangle/src/main.rs
+++ rust_book/projects/area_of_rectangle/src/main.rs
@@ -8,8 +8,17 @@
 // these are related in any ways at the moment
 
 // refactor: the tuple doesn't give us any info on what is height and
-/// what is width. let's change to a struct for more information
+// what is width. let's change to a struct for more information
 
+// addition: I want to see rect1 in an output form for debugging
+// println! does not know how to format it so errors
+// we can printusing {:?} which is debug mode (:#? is pretty debug)
+// however we need to opt into it by adding the outer attriute
+// just before struct definition. This is attached to the struct
+// could also use dbg!() which borrows and returns the values for debugging
+// rather then taking ownership
+
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -20,6 +29,8 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    println!("rect1 is {rect1:#?}");
 
     println!(
         "The area of the rectangle is {} pixels!",
