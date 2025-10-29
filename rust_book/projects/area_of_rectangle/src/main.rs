64970f2d33c03dbf1ac57091a0e7bc6ead7d8e21
Josh Walker <joshwalker.oni@gmail.com>
2025-10-29 23:28:46 +0000

    refactored to using tuples

--- rust_book/projects/area_of_rectangle/src/main.rs
+++ rust_book/projects/area_of_rectangle/src/main.rs
@@ -2,18 +2,21 @@
 // using structs to calulate area of a rectangle
 // start by using single variables then refactor
 // to use structs
-//commits will show refactor
+//commits will show refactors and comments below will show stages
+
+// refactor: change dual input to a single input, we can't see that
+// these are related in any ways at the moment
 
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} pixels!",
-        area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
